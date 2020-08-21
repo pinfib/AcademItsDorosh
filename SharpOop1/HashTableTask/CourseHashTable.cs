af0c909f3734d6729c7d1ce7a055d5e96744a4a2
pinfib <pinfib.5@gmail.com>
2020-08-22 00:42:17 +0700

    задача HashTable завершена

---
+++ SharpOop1/HashTableTask/CourseHashTable.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Academits.Dorosh.HashTableTask
+{
+    class CourseHashTable<T> : ICollection<T>
+    {
+        private List<T>[] data;
+
+        private int modCount;
+
+        private int length;
+
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    length = value;
+                }
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public CourseHashTable()
+        {
+            int capacity = 20;
+
+            data = new List<T>[capacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                data[i] = new List<T>();
+            }
+        }
+
+        public CourseHashTable(int capacity)
+        {
+            data = new List<T>[capacity];
+
+            for (int i = 0; i < capacity; i++)
+            {
+                data[i] = new List<T>();
+            }
+        }
+
+        private int GetKey(T item)
+        {
+            return Math.Abs(item.GetHashCode() % data.Length);
+        }
+
+        public void Add(T item)
+        {
+            int key = GetKey(item);
+
+            data[key].Add(item);
+
+            Count++;
+
+            modCount++;
+        }
+
+        public void Clear()
+        {
+            foreach (List<T> e in data)
+            {
+                e.Clear();
+            }
+
+            Count = 0;
+
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            int key = GetKey(item);
+
+            return data[key].Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array.Length < arrayIndex + Count)
+            {
+                throw new ArgumentException(string.Format("Целевой массив размера {0}, в него нельзя скопировать еще {1} элементов", array.Length, Count), nameof(array));
+            }
+
+            int currentIndex = arrayIndex;
+
+            foreach (List<T> e in data)
+            {
+                int listCount = e.Count;
+
+                e.CopyTo(array, currentIndex);
+
+                currentIndex += listCount;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int currentModCount = modCount;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (currentModCount != modCount)
+                {
+                    throw new InvalidOperationException("Список был изменен, нельзя продолжить цикл.");
+                }
+
+                if (data[i] != null)
+                {
+                    foreach (T e in data[i])
+                    {
+                        yield return e;
+                    }
+                }
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            int key = GetKey(item);
+
+            bool isDeleted = data[key].Remove(item);
+
+            if (isDeleted)
+            {
+                Count--;
+
+                modCount++;
+            }
+
+            return isDeleted;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder tmpString = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                tmpString.Append(i);
+                tmpString.Append(" - ");
+
+                if (data[i].Count == 0)
+                {
+                    tmpString.Append("пусто");
+                }
+                else
+                {
+                    foreach (T e in data[i])
+                    {
+                        tmpString.AppendFormat("[ {0} ] ", e);
+                    }
+                }
+
+                tmpString.AppendLine();
+            }
+
+            return tmpString.ToString();
+        }
+    }
+}
---
+++ SharpOop1/HashTableTask/HashTableProgram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Academits.Dorosh.HashTableTask
+{
+    class HashTableProgram
+    {
+        static void Main()
+        {
+            CourseHashTable<string> hashTable = new CourseHashTable<string>(5);
+
+            hashTable.Add("Москва");
+            hashTable.Add("Екатеринбург");
+            hashTable.Add("Киров");
+            hashTable.Add("Нижний Новгород");
+            hashTable.Add("Калининград");
+            hashTable.Add("Чита");
+
+            Console.WriteLine("Внутренняя организация таблицы:");
+            Console.WriteLine("Количество элементов - {0}", hashTable.Count);
+            Console.WriteLine(hashTable);
+
+            string element = "Калининград";
+            Console.Write("Содержит ли таблица строку [{0}]? ", element);
+            Console.WriteLine(hashTable.Contains(element));
+            Console.WriteLine();
+
+            try
+            {
+                string[] array = new string[20];
+                hashTable.CopyTo(array, 0);
+                Console.WriteLine("Копирование в массив. Итоговый массив: ");
+                Console.WriteLine(string.Join(" ", array));
+                Console.WriteLine();
+
+                Console.WriteLine("Тест итератора:");
+
+                foreach (string e in hashTable)
+                {
+                    Console.WriteLine("foreach: {0}", e);
+                }
+
+                Console.WriteLine();
+
+                Console.Write("Удаление элемента со значением {0}. Успешно? ", element);
+                Console.WriteLine(hashTable.Remove(element));
+                Console.WriteLine();
+
+                Console.WriteLine("Итоговое состояние таблицы:");
+                Console.WriteLine("Количество элементов - {0}", hashTable.Count);
+                Console.WriteLine(hashTable);
+                Console.WriteLine();
+
+                Console.WriteLine("Очистка таблицы.");
+                hashTable.Clear();
+                Console.WriteLine("Итоговое состояние таблицы:");
+                Console.WriteLine("Количество элементов - {0}", hashTable.Count);
+                Console.WriteLine(hashTable);
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
